e3f73debd33960cbea5225c463d004f5340e2d00
Pascal <pas111cal3@gmail.com>
2025-11-28 12:48:38 +0100

    made a simple loader for illustration fetch products

--- src/components/Admin/AdminProductGrid.jsx
+++ src/components/Admin/AdminProductGrid.jsx
@@ -1,12 +1,23 @@
-
+import {ShoppingCart} from 'lucide-react'
 import { AdminProductCard } from "./AdminProductCard"
 
 import '../../Pages/Product/ProductCardsGrid'
+
 
 export function AdminProductGrid({allProducts, setOpenEdithProduct, setEdithProduct, handleOpenEdit}){
   return(
     
     <div className='container  products-container'>
+
+        {allProducts.length === 0 && (
+        <div className="empty-cart-container">
+          <ShoppingCart size={50}/>
+          <br />
+          <div>
+            <p className='FWB text-center text-heading'>Fetching data ...</p>
+          </div>
+        </div>
+      )}
     {
       allProducts.map((product)=>{
        return( 
