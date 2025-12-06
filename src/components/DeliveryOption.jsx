c46332e930f4e1c584739ae35b971e660ff9e407
Pascal <pas111cal3@gmail.com>
2025-12-06 16:11:04 +0100

    delivery option updates with cartitem total success

--- src/components/DeliveryOption.jsx
+++ src/components/DeliveryOption.jsx
@@ -1,19 +1,32 @@
 import dayjs from "dayjs";
 import { formatMoney } from "../utils/money";
 
-export function DeliveryOption({option , cartItemId, optionIndex, handleDeliveryChange}) {
-
+export function DeliveryOption({
+  option,
+  cartItemId,
+  optionIndex,
+  handleDeliveryChange,
+  cartItem,
+}) {
   return (
     <div>
-      <input 
-      type="radio"  
-      name={`delivery-${cartItemId}`} 
-      checked={option.isActive|| false}
-       onChange={() => handleDeliveryChange(cartItemId, optionIndex)}
+      <input
+        type="radio"
+        name={`delivery-${cartItemId}`}
+        value={dayjs(option.date).format("dddd MMMM D")}
+        checked={cartItem.selectedDeliveryOption === optionIndex}
+        onChange={() => {
+          handleDeliveryChange(cartItemId, optionIndex);
+        }}
       />
       <p className="FWB">
         <span>{dayjs(option.date).format("dddd MMMM D")}</span>
-        <span className="text-grey d-block"> {option.price === 0 ? "Free - Shipping" : `${formatMoney(option.price)} - Shipping`}</span>
+        <span className="text-grey d-block">
+          {" "}
+          {option.price === 0
+            ? "Free - Shipping"
+            : `${formatMoney(option.price)} - Shipping`}
+        </span>
       </p>
     </div>
   );
