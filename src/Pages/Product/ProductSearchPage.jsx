59dbeed719d9237ed7bfe4e8e4a7554f972a09e9
Pascal <pas111cal3@gmail.com>
2026-01-21 16:45:49 +0100

    mage product search page responsive

--- src/Pages/Product/ProductSearchPage.jsx
+++ src/Pages/Product/ProductSearchPage.jsx
@@ -1,8 +1,7 @@
 import { SideBarHeader } from "../../components/SideBarHeader";
 import { useSearchParams } from "react-router";
 import { useState, useEffect } from "react";
-import { ProductCardsGrid } from "./ProductCardsGrid";
-import { PackageSearch } from "lucide-react";
+import { PackageSearch, LoaderCircle, ListFilterPlus } from "lucide-react";
 import "./ProductSearchPage.css";
 import "./ProductCards.css";
 import { ProductCard } from "./ProductCard";
@@ -17,22 +16,21 @@ export function ProductSearchPage({ handleGetCartAPI, cartLength }) {
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [isFilter, setIsFilter] = useState(false);
 
   const discountPercentage = [0.5, 0.4, 0.3, 0.2, 0.1];
 
   const query = searchParams.get("q");
-const priceParam = searchParams.get("price");
-const discountParam = searchParams.get("discount");
+  const priceParam = searchParams.get("price");
+  const discountParam = searchParams.get("discount");
 
-const price =
-  priceParam !== null ? Number(priceParam) : undefined;
+  const price = priceParam !== null ? Number(priceParam) : undefined;
 
-const discount =
-  discountParam !== null ? Number(discountParam) : undefined;
+  const discount = discountParam !== null ? Number(discountParam) : undefined;
 
   const [seclectedDiscountPercent, setSelectedDiscountPercent] =
     useState(discount);
-  const [priceRange, setPriceRange] = useState(price?? 1000000);
+  const [priceRange, setPriceRange] = useState(price ?? 1000000);
 
   // const [tempPrice, setTempPrice] = useState(price);
 
@@ -63,9 +61,10 @@ const discount =
   return (
     <>
       <SideBarHeader cartLength={cartLength} />
+
       <div className="product-search-page-container container ">
         <div className="filter-and-search-product-container">
-          <div className="filter-container">
+          <div className={`filter-container ${isFilter ? "show" : ""}`}>
             <div className=" w100">
               <div>
                 <div className="d-flex align-center justify-s-between">
@@ -76,8 +75,9 @@ const discount =
                         prev.set("price", priceRange); // Only updates URL when clicked
                         return prev;
                       });
+                      setIsFilter(false);
                     }}
-                    className="bg-transparent text-link"
+                    className="bg-transparent text-link apply-btn"
                   >
                     Apply
                   </button>
@@ -114,6 +114,7 @@ const discount =
                           return prev;
                         });
                         setSelectedDiscountPercent(percent);
+                        setIsFilter(false);
                       }}
                     />
                     {percent * 100}% or more
@@ -129,38 +130,71 @@ const discount =
                   params.delete("discount");
                   return params;
                 });
+                setIsFilter(false);
               }}
               className="text-white bg-heading"
+              disabled={loading}
             >
               <ListFilter /> Clear Filter
             </button>
           </div>
 
-          <div className="d-flex flex-1 flex-column justify-center ">
-            <p>
-              search result matching , name:{" "}
-              <span className="FWB">{query},</span> &nbsp; price-below:
-              <span className="FWB"> { price !== undefined && formatMoney(price)},</span> &nbsp;
-              discount-below : <span className="FWB">{ discount !== undefined && discount * 100}%</span>
-            </p>
-            {searchResults.length === 0 ? (
-              <div className="d-flex flex-1 flex-column align-center justify-center">
-                <PackageSearch size={100} />
-                <h2 className="text-center">Search for products</h2>
-              </div>
-            ) : (
-              <div className="search-product-container">
-                {searchResults.map((product) => {
-                  return (
-                    <ProductCard
-                      key={product._id}
-                      product={product}
-                      handleGetCartAPI={handleGetCartAPI}
-                    />
-                  );
-                })}
+          <div className="d-flex flex-1 flex-column justify-center product-wrapper">
+            <div className="d-flex justify-s-between align-center">
+              <small>
+                search result matching &gt;
+                <span className="FWB text-green">{query}</span> &nbsp;
+                {price !== undefined && (
+                  <>
+                    price-below &gt;{" "}
+                    <span className="FWB text-green">{formatMoney(price)}</span>
+                  </>
+                )}
+                , &nbsp;
+                {discount !== undefined && (
+                  <>
+                    {" "}
+                    discount-below &gt;{" "}
+                    <span className="FWB text-green"> {discount * 100}%</span>
+                  </>
+                )}
+              </small>
+              <button
+                disabled={loading}
+                className={`mobile-display-filter-btn `}
+                onClick={() => {
+                  setIsFilter((prev) => !prev);
+                }}
+              >
+                filter
+                <ListFilterPlus size={20} />
+              </button>
+            </div>
+            {/* LOADING STATE */}
+            {loading && (
+              <div className="d-flex flex-1 align-center justify-center">
+                <LoaderCircle size={50} className="spin text-heading" />
               </div>
             )}
+            {!loading &&
+              (searchResults.length === 0 ? (
+                <div className="d-flex flex-1 flex-column align-center justify-center">
+                  <PackageSearch size={100} />
+                  <h2 className="text-center">Search for products</h2>
+                </div>
+              ) : (
+                <div className="search-product-container">
+                  {searchResults.map((product) => {
+                    return (
+                      <ProductCard
+                        key={product._id}
+                        product={product}
+                        handleGetCartAPI={handleGetCartAPI}
+                      />
+                    );
+                  })}
+                </div>
+              ))}
           </div>
         </div>
         <Footer />
