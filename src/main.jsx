af4bb64c871e448d11d361949cd0293894d681b9
Pascal <pas111cal3@gmail.com>
2026-02-22 21:29:00 +0100

    feat: add scroll top feat

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -9,8 +10,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </AuthProvider>
-  </StrictMode>
+  </StrictMode>,
 );
